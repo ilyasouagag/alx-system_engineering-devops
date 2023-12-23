a72ea467892ccf2cd908d887aa4baa961331e536
ilyasouagag <ilyasouagag2011@gmail.com>
2023-12-23 01:41:33 +0100

    Zombie like simple shell

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -3,31 +3,39 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-
+/**
+ * infinite_while - keep sleep
+ * Return: return 0
+ */
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
+/**
+ * main - the main function
+ * Return: return exit_success
+ */
 int main(void)
 {
-    pid_t pid;
-    char count = 0;
-    while (count < 5)
-    {
-        pid = fork();
-        if (pid > 0)
-        {
-            printf("Zombie process created, PID: %d\n", pid);
-            sleep(1);
-            count++;
-        }
-        else
-            exit(0);
-    }
-    infinite_while();
-    return (EXIT_SUCCESS);
+	pid_t pid;
+	char count = 0;
+
+	while (count < 5)
+	{
+		pid = fork();
+		if (pid > 0)
+		{
+			printf("Zombie process created, PID: %d\n", pid);
+			sleep(1);
+			count++;
+		}
+		else
+			exit(0);
+	}
+	infinite_while();
+	return (EXIT_SUCCESS);
 }
