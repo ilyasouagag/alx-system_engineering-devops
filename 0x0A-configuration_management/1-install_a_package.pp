7eb0c120eedc94f3e61230fb52a9c16977db1196
ilyasouagag <ilyasouagag2011@gmail.com>
2024-02-23 22:18:53 +0100

    installing flask frompip3 using puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/pup
-# Installing flask from pip3 using Puppet
+# Install an especific version of flask (2.1.0)
 package {'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3'
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
