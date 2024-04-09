0ca175b808b80c1fb9353d3a03369eb4091b9582
ilyasouagag <ilyasouagag2011@gmail.com>
2024-04-09 21:37:25 +0000

    api advanced

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -13,6 +13,8 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
         after (str): The parameter for the next page of the API results.
         count (int): The parameter of results matched thus far.
     """
+    if not word_list or word_list == [] or not subreddit:
+        return
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
